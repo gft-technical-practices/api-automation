dc049c9e8c33dc9b1f4980dfa92ebed78c36ef2f
rmzoni <rmzoni@gmail.com>
2017-09-03 21:18:32 -0300

    Starting create env for ubuntu

---
+++ create_env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install Jenkins
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | apt-key add -
+echo deb http://pkg.jenkins.io/debian-stable binary/ | tee /etc/apt/sources.list.d/jenkins.list
+apt-get update
+apt-get install jenkins
+
+# Add the jenkins user to the docker group. Permission to create images
+usermod -a -G docker jenkins
+
+# Starting and Making Jekins on Startup Default
+systemctl start jenkins
+systemctl enable jenkins
+systemctl status jenkins
