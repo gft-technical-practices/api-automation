b30835715fa9d92342948ca18cd94a8f4be52b43
rmzoni <rmzoni@gmail.com>
2017-09-03 22:25:45 -0300

    Updating pre reqs

--- create_env.sh
+++ create_env.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Pre Reqs
+apt-get update
+apt-get upgrade
+apt-get install git docker docker-compose openjdk-8-jdk maven
+
+# Docker Configuration
+groupadd docker
+chown root:docker /var/run/docker.sock
+usermod -a -G docker $USER
+systemctl start docker
+systemctl enable docker
+
 # Install Jenkins
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | apt-key add -
 echo deb http://pkg.jenkins.io/debian-stable binary/ | tee /etc/apt/sources.list.d/jenkins.list
