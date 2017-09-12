98ef9de5c6ee441876518838eb46cc876d0cc898
Rafael Manzoni <rmzoni@gmail.com>
2017-09-12 15:44:22 -0700

    Creating jenkins file for pipeline

---
+++ jenkins/api-automation-pipeline/jenkinsfile.groovy
@@ -0,0 +1,54 @@
+node {
+
+   stage('Preparation') {
+      
+      // Clean Workspace
+      cleanWs()
+      
+      // Get some code from a GitHub repository
+      git 'https://github.com/api-design-automation/user-api'
+
+      // Convert Swagger Definition from YAML to JSON
+      sh '''#!/bin/bash
+
+            # Convert YAML Swagger to JSON Swagger
+            yaml2json ./api.yaml --pretty > ./api.json
+        '''    
+   }
+
+   stage('Scaffolding') {
+       // Run scripts to scaffolding the project
+       sh '''#!/usr/bin/env node
+            const shell = require(\'shelljs\');
+            const swg = require(\'api-scaffolding\');
+            const fs = require(\'fs-extra\');
+            const path = require(\'path\');
+            const asciify = require(\'asciify\');
+
+            const spec = fs.readJsonSync(path.resolve(\'./api.json\'));
+
+            // Criando as apis na versão server
+            shell.echo(swg.createServer(spec, \'nodejs-server\'));
+
+
+            asciify(\'Scaffolding\', {font:\'small\'}, (err, res) => {shell.echo(res)});
+            asciify(\'Create APIs\', {font:\'standard\', color: \'blue\'}, (err, res) => {shell.echo(res)});
+          '''
+
+        // Clean
+        sh '''#!/bin/bash
+                mv ${WORKSPACE}/nodejs-server-server/* ${WORKSPACE}
+                rm -rf ${WORKSPACE}/nodejs-server-server
+                rm -rf ${WORKSPACE}/download
+            '''
+
+   }
+
+   stage('Build') {
+       docker.withRegistry('https://registry.hub.docker.com', 'rmzoni-dockerhub') {
+            def customImage = docker.build("rmzoni/${env.API_NAME}:${env.BUILD_ID}")
+            customImage.push()
+       }
+       
+   }
+}
