96a01b2077fde57ee578ad9223fbe5c4024d5cb6
rmzoni <remi@gft.com>
2017-09-21 19:39:51 -0300

    Dynamic file name for api swagger

--- jenkins/api-automation-pipeline/convert_yaml_json.sh
+++ jenkins/api-automation-pipeline/convert_yaml_json.sh
@@ -3,4 +3,4 @@ set -x #echo on
 
 # Convert YAML Swagger to JSON Swagger
 API_FILE_NAME=`ls | grep .yaml | head -n 1`
-yaml2json ./${API_FILE_NAME}.yaml --pretty > ./api.json
+yaml2json ./${API_FILE_NAME} --pretty > ./api.json
